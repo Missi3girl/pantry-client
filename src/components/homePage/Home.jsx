202cf8d520cd647e1ac1ab336eb0e44a1bcfbaf8
Ronnie Ingram <ringramjr6@gmail.com>
2025-07-08 19:36:38 -0400

    new Home branch

--- src/components/homePage/Home.jsx
+++ src/components/homePage/Home.jsx
@@ -1,1 +1,45 @@
-// Entry point for route with imported components for the Home page
+import React from 'react'
+import './Home.css';
+
+function home() {
+  return (
+    <div className="home-container">
+        <h1 className="home-header">The Pantry Door</h1>
+
+        <div className="nav-bar">
+            <div className="nav-item">Find a Bank</div>
+             <div className="nav-item">Get Involved</div>
+             <div className="nav-item">About</div>
+         <div className="nav-item">Contact</div>
+        </div>
+
+        <div className="main-content">
+          <div className="location-card"></div>
+            <div className="location-card"></div>
+            <div className="location-card"></div>
+            <div className="location-card"></div>
+            <div className="location-card"></div>
+        </div>
+
+    <div className="map-section">
+        <div className="map-box">MAP</div>
+        <div className="search-bar">
+            <label>SEARCH</label>
+            <input type="text" placeholder="Search Pantry Location"/>
+            <button>GO</button>
+         </div>
+        </div>
+    
+    <div>
+        <footer> Canned Do Crew, 2025 </footer>
+    
+    </div>
+    
+ </div>
+
+
+
+  );
+}
+
+export default home;
