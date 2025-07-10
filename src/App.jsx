11483f9e0381014d366b8afb8007baf1348c22ab
Ronnie Ingram <ringramjr6@gmail.com>
2025-07-09 22:15:29 -0400

    updated home and added to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css'
 import Header from './components/headerFooterPage/Header'
 import Footer from './components/headerFooterPage/Footer'
 import About from './components/AboutPage/About'
+import Home from './components/homePage/Home'
 
 function App() {
 
     return (
         <>
               <Header />
+              <Home />
               <About />
               <Footer />
         </>
--- src/components/homePage/Home.jsx
+++ src/components/homePage/Home.jsx
@@ -4,17 +4,16 @@ import './Home.css';
 function Home() {
   return (
     <div className="home-container">
-      <h1 className="home-header">The Pantry Door</h1>
+      {/* <h1 className="home-header">The Pantry Door</h1> */}
 
-      /* Nav Bar */
-      <div className="nav-bar">
+      
+      {/* <div className="nav-bar">
         <a className="nav-item" href="/find-a-bank">Find a Bank</a>
         <a className="nav-item" href="/get-involved">Get Involved</a>
         <a className="nav-item" href="/about">About</a>
         <a className="nav-item" href="/contact">Contact</a>
-      </div>
+      </div> */}
 
-      /* Main Content (Locations + Map) */
       <div className="main-content">
         <div className="locations-list">
           <div className="location-card">Location 1</div>
@@ -24,19 +23,17 @@ function Home() {
           <div className="location-card">Location 5</div>
         </div>
 
-        /* Map and Search */
         <div className="map-section">
           <div className="map-box">MAP</div>
           <div className="search-bar">
             <label>SEARCH</label>
-            <input type="text" placeholder="Search Pantry Location" />
+            <input type="text" placeholder="Search Pantry Location By Zip Code" />
             <button>GO</button>
           </div>
         </div>
       </div>
 
       
-      <footer className="footer">Canned Do Crew, 2025</footer>
     </div>
   );
 }
