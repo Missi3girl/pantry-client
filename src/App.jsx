4b925782f3f98b2a34bec447c67ea6a4cf51e069
Missy Eleazer <angelfish814@gmail.com>
2025-07-09 22:48:20 -0400

    getInvlvdSvc.js-done, started InvlvForm.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { useState, useEffect } from 'react'
 import './App.css'
+import InvlvForm from './components/getInvolvedPage/InvlvForm'
 
 function App() {
 
     return (
         <>
             <h1>Welcome to your front end</h1>
+            <InvlvForm /> 
         </>
     )
 }
 
 export default App
+
+
+// ! I do not like the layout of the form and may need to dive deeper into what it means to link the form with backend because I sent the form but it rendered nothing inside the database, and neither did my newest email from Frankie (sent via postman)
--- src/components/getInvolvedPage/InvlvForm.jsx
+++ src/components/getInvolvedPage/InvlvForm.jsx
@@ -1,1 +1,48 @@
-// get involved form to volunteer/submit pantry info
+import { useState } from 'react';
+import { submitGetInvolved } from '../../services/getInvlvdSvc';
+
+export default function InvlvForm() {
+    const [form, setForm] = useState({
+        name: "",
+        email: "",
+        phoneNumber: "",
+        message: "",
+        reason: "" // a dropdown menu
+    });
+    // handles the updates made to just the fields that are actually changed. In our app, this form is for new messages/submissions, and all fields, but one, are required. Still, this will handle each field as it changes from "" to having data.
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setForm((prev) => ({ ...prev, [name]: value }));
+    };
+
+    // handles what happens when the form is submitted by user
+    const handleSubmit = async (e) => {
+        e.preventdefault();
+
+        try {
+            const response = await submitGetInvolved(form);
+            console.log("Success!", response);
+        } catch (err) {
+            console.error("Error while submitting:", err);
+        }
+    };
+
+  return (
+    <div>
+        <form onSubmit={handleSubmit}>
+            <select name="reason" onChange={handleChange} value={form.reason}>
+                <option value="">Select a reason</option>
+                <option value="help">I need help</option>
+                <option value="volunteer">I want to volunteer</option>
+                <option value="new">New pantry suggestion</option>
+                <option value="update">Update a current panrty</option>
+            </select>
+            <input name="name" onChange={handleChange} value={form.name} />
+            <input name="email" onChange={handleChange} value={form.email} />
+            <input name="phoneNumber" onChange={handleChange} value={form.phoneNumber} />
+            <textarea name="message" onChange={handleChange} value={form.message}></textarea>
+            <button type="submit">Send</button>
+        </form>
+    </div>
+  );
+}
