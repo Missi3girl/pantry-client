c50eeb6dc06bf2c075e7a3bcb0a5606d3c0a657d
Ronnie Ingram <ringramjr6@gmail.com>
2025-07-13 08:21:23 -0400

    added map.jsx comp, need to import to app.jsx

--- src/components/homePage/Map.jsx
+++ src/components/homePage/Map.jsx
@@ -1,1 +1,41 @@
 // UI component that displays the visual/interactive map
+
+import React, {useRef, useEffect, useState} from 'react'
+import mapboxgl from 'mapbox-gl'
+import { Zoom } from '@mui/material';
+import './Map.css'
+
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiY2FubmVkZG9jcmV3IiwiYSI6ImNtY3dpbnJhYzAycGYybHBydGpmdHBpdG8ifQ.y3jyrF-ho57nklyYVfocWA';
+
+const Map = () => {
+    const mapContainer = useRef(null);
+    const mapBox = useRef(null);
+    const [lng, setLng] = useState(84.38); 
+    const [lat, setLat] = useState(33.75);
+    const [zoom, setZoom] = useState(10);
+
+    useEffect(() => {
+        if (mapBox.current) return;
+
+        mapBox.current = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/streets-v12',
+            center: [lng, lat], 
+            zoom: zoom
+        });
+
+        return () => mapBox.current.remove();
+     }, []);
+
+     return (
+        <div>
+            <div ref={mapContainer} style={{ width: '80%', height: '500px' }} />
+            <div className="sidebar">
+                Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+            </div>
+        </div>
+    );
+};
+
+export default Map;
