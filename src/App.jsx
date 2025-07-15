115c0d43e41c528c4cec976e9afdc6e7688fc9fe
Ronnie Ingram <ringramjr6@gmail.com>
2025-07-15 13:53:43 -0400

    merged app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Footer from './components/headerFooterPage/Footer'
 import About from './components/aboutPage/About'
 import GetInvolved from './components/getInvolvedPage/GetInvolved'
 import Home from './components/homePage/Home'
+import RsrcList from './components/resourcesPage/RsrcList'
 
 function App() {
 
@@ -24,3 +25,5 @@ function App() {
 }
 
 export default App
+
+
